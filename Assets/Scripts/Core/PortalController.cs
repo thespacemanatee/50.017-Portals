08810aebeeddc56c447eae4d1173251e8416c840
Chee Kit <cheekit.chong98@gmail.com>
2022-06-28 14:01:56 +0800

    chore: restructure project

---
+++ Assets/Scripts/Core/PortalController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Core
+{
+    public class PortalController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Core/PortalUser.cs
@@ -0,0 +1,61 @@
+using System.Linq;
+using UnityEngine;
+
+namespace Core
+{
+    public class PortalUser : MonoBehaviour
+    {
+        private static readonly int SliceNormal = Shader.PropertyToID("sliceNormal");
+        private static readonly int SliceOffsetDst = Shader.PropertyToID("sliceOffsetDst");
+        public GameObject originalGameObject;
+        public GameObject GraphicsClone { get; set; }
+        public Vector3 PreviousOffsetFromPortal { get; set; }
+        public Material[] OriginalMaterials { get; set; }
+        public Material[] CloneMaterials { get; set; }
+
+        public virtual void Teleport(Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot)
+        {
+            transform.position = pos;
+            transform.rotation = rot;
+        }
+
+        // Called when first touches portal
+        public virtual void EnterPortalThreshold()
+        {
+            if (GraphicsClone == null)
+            {
+                GraphicsClone = Instantiate(originalGameObject, originalGameObject.transform.parent, true);
+                GraphicsClone.transform.localScale = originalGameObject.transform.localScale;
+                OriginalMaterials = GetMaterials(originalGameObject);
+                CloneMaterials = GetMaterials(GraphicsClone);
+            }
+            else
+            {
+                GraphicsClone.SetActive(true);
+            }
+        }
+
+        // Called once no longer touching portal (excluding when teleporting)
+        public virtual void ExitPortalThreshold()
+        {
+            GraphicsClone.SetActive(false);
+            // Disable slicing
+            foreach (var t in OriginalMaterials) t.SetVector(SliceNormal, Vector3.zero);
+        }
+
+        public void SetSliceOffsetDst(float dst, bool clone)
+        {
+            for (var i = 0; i < OriginalMaterials.Length; i++)
+                if (clone)
+                    CloneMaterials[i].SetFloat(SliceOffsetDst, dst);
+                else
+                    OriginalMaterials[i].SetFloat(SliceOffsetDst, dst);
+        }
+
+        private static Material[] GetMaterials(GameObject g)
+        {
+            var renderers = g.GetComponentsInChildren<MeshRenderer>();
+            return renderers.SelectMany(renderer => renderer.materials).ToArray();
+        }
+    }
+}
