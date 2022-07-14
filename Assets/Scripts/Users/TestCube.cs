905d2d0ea2bb7e7cf9763efb291c3b0e1b3023be
Chee Kit <cheekit.chong98@gmail.com>
2022-07-14 18:55:49 +0800

    chore: working moving demo

--- Assets/Scripts/Users/TestCube.cs
+++ Assets/Scripts/Users/TestCube.cs
@@ -1,8 +1,42 @@
 ﻿using Core;
+using UnityEngine;
 
 namespace Users
 {
     public class TestCube : PortalUser
     {
+        public float maxSpeed = 1;
+        float speed;
+        float targetSpeed;
+        float smoothV;
+        private bool isMoving;
+
+        void Start()
+        {
+            Debug.Log("Press C to stop/start car");
+            targetSpeed = maxSpeed;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                isMoving = true;
+            }
+
+            if (isMoving)
+            {
+                float moveDst = Time.deltaTime * speed;
+                transform.position += transform.forward * Time.deltaTime * speed;
+
+                if (Input.GetKeyDown(KeyCode.C))
+                {
+                    targetSpeed = (targetSpeed == 0) ? maxSpeed : 0;
+                }
+
+                speed = Mathf.SmoothDamp(speed, targetSpeed, ref smoothV, .5f);
+            }
+        }
     }
 }
